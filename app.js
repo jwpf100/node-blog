d3493fd4c88149e7b4de748901d9901f8b10b81a
jwpf100 <jwpf100@gmail.com>
2020-10-27 09:58:42 +0000

    Finished routes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ dotenv.config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var blogRouter = require('./routes/blog');
 
 // Create App
 
@@ -52,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blog', blogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
