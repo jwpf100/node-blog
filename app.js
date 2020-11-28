36fe69f4203a629580229bd5848c8f99a096c1c3
jwpf100 <jwpf100@gmail.com>
2020-11-28 15:57:53 +0000

    Added compression middleware.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 //manage .env data
 const dotenv = require('dotenv');
@@ -51,6 +52,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Add (previously imported) route handling
