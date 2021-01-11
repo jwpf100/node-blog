d23637163cf005a4787c19f355a4a25c19f46d17
jwpf100 <jwpf100@gmail.com>
2021-01-11 12:13:11 +0000

    Added dev and prod DB.

--- app.js
+++ app.js
@@ -33,8 +33,10 @@ const mongoose = require('mongoose');
 //Cloud Atlas
 //dev_db_url is the 'development' database.
 const dev_db_url = 'mongodb://127.0.0.1:27017/node-blog';
-//const pro_db_url = `mongodb+srv://${process.env.CLOUDDB_USERNAME}:${process.env.CLOUDDB_PASSWORD}@nodeblogjoe.iuune.mongodb.net/<dbname>?retryWrites=true&w=majority`;
-const pro_db_url = 'mongodb+srv://jwpf100:Y3yHZ9wRqbBjQCsa@nodeblogjoe.iuune.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const pro_db_url = `mongodb+srv://${process.env.CLOUDDB_USERNAME}:${process.env.CLOUDDB_PASSWORD}@nodeblogjoe.iuune.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+//const pro_db_url =
+//  'mongodb+srv://jwpf100:Y3yHZ9wRqbBjQCsa@nodeblogjoe.iuune.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
 //const url = process.env.MONGODB_URI || dev_db_url;
 const url = pro_db_url || dev_db_url;
 
