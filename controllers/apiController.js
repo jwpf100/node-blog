25da9c937780fc32f817c7fa5ecb13ab9698558b
jwpf100 <jwpf100@gmail.com>
2021-01-12 09:34:20 +0000

    Minor change.

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,7 +4,7 @@ const async = require('async');
 
 //Display list of all blog posts
 exports.blogpost_list = function (req, res) {
-  BlogPost.find({}, 'title author')
+  BlogPost.find({}, 'title summary author')
     .populate('author')
     .exec(function (err, list_blogposts) {
       if (err) {
